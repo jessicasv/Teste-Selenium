2aebf069eb022269658009332be1e5c53f817bde
jessicasv <jessicavianasv@gmail.com>
2022-09-12 13:54:20 -0300

    Criando classe Utils

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -4,8 +4,9 @@ import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import support.Utils;
 
-public class HomePage {
+public class HomePage extends Utils {
 
     WebDriver driver;
     public HomePage(WebDriver driver) {
@@ -15,6 +16,7 @@ public class HomePage {
     public void acessarAplicacao() {
         driver.manage().window().maximize();
         driver.get("https://qazando.com.br/curso.html");
+        esperarElementoEstarPresente(By.id("btn-ver-cursos"), 10);
         Assert.assertEquals("Não acessou a aplicação!",true, driver.findElement(By.id("btn-ver-cursos")).isDisplayed());
     }
 
